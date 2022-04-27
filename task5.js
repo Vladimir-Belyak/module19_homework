54ee9ac023eff92bb9d2a6989f57f9efb0af8a55
Vladimir-Belyak <wabelyak@mail.ru>
2022-04-27 23:24:50 +0700

    2nd upload, 4/5 modify

--- task5.js
+++ task5.js
@@ -1,55 +1,58 @@
-function Devices(name, power) {
-    this.name = name,
-        this.power = power,
-        this.battery = 'No',
-        this.color = 'Grey'
-}
-
-Devices.prototype.getPower = function () {
-    console.log(`Power is ${this.power} W`)
-}
-
-function Computers(name, power, display) {
-    this.name = name,
-        this.power = power,
-        this.display = display
-}
-
-function Lamps(name, power) {
-    this.name = name,
-    this.power = power
-}
-
-function LampsBattery(name, power, battery) {
-    this.name = name,
-    this.power = power,
-      this.battery = battery
-}
-
-Devices.prototype.showKeyProperty = function () {
-      for (let key in this) {
-        if (this.hasOwnProperty(key))
-    console.log(`${key}: ${this[key]}`)
+class Devices {
+    constructor(name, power) {
+        this.name = name,
+            this.power = power,
+            this.battery = 'No',
+            this.color = 'Grey'
+    }
+    getPower() {
+        console.log(`Power is ${this.power} W`)
+    }
+    showKeyProperty() {
+        for (let key in this) {
+            if (this.hasOwnProperty(key))
+                console.log(`${key}: ${this[key]}`)
+        }
     }
 }
 
-Computers.prototype = new Devices();
-Lamps.prototype = new Devices();
-LampsBattery.prototype = new Lamps();
+class Computers extends Devices {
+    constructor(name, power, display) {
+        this.name = name,
+            this.power = power,
+            this.display = display
+    }
+}
 
-Lamps.prototype.getPower = function () {
-    console.log(`Power is ${this.power} W(Light-emitting diode)`)
+class Lamps extends Devices {
+    constructor(name, power) {
+        this.name = name,
+            this.power = power
+    }
+    getPower() {
+        console.log(`Power is ${this.power} W(Light-emitting diode)`)
+    }
+}
+
+class LampsBattery extends Lamps {
+    constructor(name, power, battery) {
+        this.name = name,
+            this.power = power,
+            this.battery = battery
+    }
 }
 
 const computer = new Computers('Office_PC', '500', '22');
 const lamp = new Lamps('Bra', '30');
-const mobile_lamp = new LampsBattery('Flashlight' ,'10', 'Yes');
+const mobile_lamp = new LampsBattery('Flashlight', '10', 'Yes');
 
 lamp.getPower()
 computer.getPower()
 mobile_lamp.showKeyProperty()
 
+
 /*
+Перепишите консольное приложение из предыдущего юнита на классы.
 Реализуйте следующее консольное приложение подобно примеру, который разбирался в видео. Реализуйте его на прототипах.
 Определите иерархию электроприборов. Включите некоторые в розетку. Посчитайте потребляемую мощность (передайте аргумент). 
 Таких приборов должно быть как минимум два (например, настольная лампа и компьютер). Выбрав прибор, подумайте, 
